bcc68b94eae0104a1a0f74ab2692f4da27e5d01a
Frizlab <fload@me.com>
2018-11-12 14:47:40 +0100

    Reimplement C++ code from Stackoverflow in Swift

--- Sources/RecursiveSyncDispatch/RecursiveSyncDispatch.swift
+++ Sources/RecursiveSyncDispatch/RecursiveSyncDispatch.swift
@@ -7,18 +7,53 @@
  */
 
 import Dispatch
+import Foundation
 
 
 
 extension DispatchQueue {
 	
+	/* Not implemented yet. (Among other, how do we process the flags?) */
 //	@available(OSX 10.10, iOS 8.0, *)
 //	public func recursiveSync(execute workItem: DispatchWorkItem)
 	
 	public func recursiveSync<T>(execute work: () throws -> T) rethrows -> T {
+		if queueStackCheck(mode: .push) {
+			return try work()
+		} else {
+			let ret = try sync(execute: work)
+			_ = queueStackCheck(mode: .pop)
+			return ret
+		}
+	}
+	
+	/* Not implemented yet. (How do we process the flags?) */
+//	public func recursiveSync<T>(flags: DispatchWorkItemFlags, execute work: () throws -> T) rethrows -> T
+	
+	/* ***************
+      MARK: - Private
+	   *************** */
+	
+	private enum QueueStackCheckMode {
+		
+		case push
+		case pop
 		
 	}
 	
-//	public func recursiveSync<T>(flags: DispatchWorkItemFlags, execute work: () throws -> T) rethrows -> T
+	private static let queuesPerThreadKey = "FLRSD_QueuesWithRecursiveDispatchOnThread"
+	
+	private func queueStackCheck(mode: QueueStackCheckMode) -> Bool {
+		var queuesSet = Thread.current.threadDictionary[DispatchQueue.queuesPerThreadKey] as! Set<DispatchQueue>? ?? Set()
+		
+		var modified = false
+		switch mode {
+		case .push: modified =  queuesSet.insert(self).inserted
+		case .pop:  modified = (queuesSet.remove(self) != nil)
+		}
+		
+		if modified {Thread.current.threadDictionary[DispatchQueue.queuesPerThreadKey] = queuesSet}
+		return !modified
+	}
 	
 }
--- Tests/RecursiveSyncDispatchTests/RecursiveSyncDispatchTests.swift
+++ Tests/RecursiveSyncDispatchTests/RecursiveSyncDispatchTests.swift
@@ -5,11 +5,47 @@ import XCTest
 
 final class RecursiveSyncDispatchTests: XCTestCase {
 	
-	func testExample() {
+	func testBasicUsage() {
+		var wentIn = false
+		let a = DispatchQueue(label: "a")
+		
+		a.recursiveSync{
+			a.recursiveSync{
+				wentIn = true
+			}
+		}
+		
+		XCTAssertTrue(wentIn)
+	}
+	
+	func testSimpleUsage() {
+		var wentIn = false
+		let a = DispatchQueue(label: "a")
+		let b = DispatchQueue(label: "b")
+		let c = DispatchQueue(label: "c")
+		
+		a.recursiveSync{
+			b.recursiveSync{
+				c.recursiveSync{
+					a.recursiveSync{
+						b.recursiveSync{
+							c.recursiveSync{
+								a.recursiveSync{
+									wentIn = true
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		XCTAssertTrue(wentIn)
 	}
 	
 	static var allTests = [
-		("testExample", testExample),
+		("testBasicUsage", testBasicUsage),
+		("testSimpleUsage", testSimpleUsage),
 	]
 	
 }
